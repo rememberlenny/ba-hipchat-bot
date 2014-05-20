dbf512d396ca39dcbcc79eb1702d903b5a20b70c
RememberLenny <leonard_bogdonoff@condenast.com>
2014-05-20 13:25:45 -0400

    Added newrelic for some reason

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name : ['My Application'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key : '40a13e79c3349724f0c7788bbea0fe609855dd86',
+  logging : {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level : 'trace'
+  }
+};
